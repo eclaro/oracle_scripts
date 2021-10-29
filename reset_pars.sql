e8b8811cf625f05f8e0142fbd50a0d8ba2447dfd
eclaro <37638034+eclaro@users.noreply.github.com>
2021-10-29 18:31:23 -0500

    Add files via upload

---
+++ reset_pars.sql
@@ -0,0 +1,46 @@
+--http://orasql.org/2013/03/29/sqlplus-tips-1/
+
+--undef 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+
+define 1=""
+define 2=""
+define 3=""
+define 4=""
+define 5=""
+define 6=""
+define 7=""
+define 8=""
+define 9=""
+define 10=""
+define 11=""
+define 12=""
+define 13=""
+define 14=""
+define 15=""
+define 16=""
+
+/*
+set termout off
+COLUMN  1  NEW_VALUE  1    noprint
+COLUMN  2  NEW_VALUE  2    noprint
+COLUMN  3  NEW_VALUE  3    noprint
+COLUMN  4  NEW_VALUE  4    noprint
+COLUMN  5  NEW_VALUE  5    noprint
+COLUMN  6  NEW_VALUE  6    noprint
+COLUMN  7  NEW_VALUE  7    noprint
+COLUMN  8  NEW_VALUE  8    noprint
+COLUMN  9  NEW_VALUE  9    noprint
+COLUMN 10  NEW_VALUE 10    noprint
+COLUMN 11  NEW_VALUE 11    noprint
+COLUMN 12  NEW_VALUE 12    noprint
+COLUMN 13  NEW_VALUE 13    noprint
+COLUMN 14  NEW_VALUE 14    noprint
+COLUMN 15  NEW_VALUE 15    noprint
+COLUMN 16  NEW_VALUE 16    noprint
+SELECT ''  "1", '' "5", ''  "9", '' "13"
+      ,''  "2", '' "6", '' "10", '' "14"
+      ,''  "3", '' "7", '' "11", '' "15"
+      ,''  "4", '' "8", '' "12", '' "16"
+  FROM dual;
+set termout on;
+*/
